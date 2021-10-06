1e51d4ba44772428e46bee85b14680b457508cf2
dev-haro <harold.da-costa@epitech.eu>
2021-10-06 17:56:46 +0100

    buco

--- buco/urls.py
+++ buco/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('landinpage.urls', namespace='landinpage')),
+    path('', include('landinpage.urls', namespace='landinpage')),
 ]
 
 if settings.DEBUG:
